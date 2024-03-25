498af1957fae979ad6578b535580cdd423910db4
sourav <souravchongrey@gmail.com>
2024-03-25 11:28:43 +0530

    simple search items or products

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import "./App.css";
+import product from "./constant";
 function App() {
+  const [search, setSearch] = React.useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mx-auto max-w-7xl">
+      <div className="  flex justify-center items-center py-10">
+        <div>
+          <input
+            type="text"
+            placeholder="Search something for "
+            className="p-2 px-10 "
+            onChange={(e) => setSearch(e.target.value)}
+          />
+        </div>
+      </div>
+      <div className="grid grid-cols-3 gap-8">
+        {product
+          .filter((val) => {
+            if (search === "" || search === null) {
+              return val;
+            } else if (
+              val.product_name.toLowerCase().includes(search.toLowerCase())
+            ) {
+              return val;
+            }
+          })
+          .map((item, index) => (
+            <div key={index} className="max-w-full bg-green-400 flex flex-col justify-center items-center rounded-lg shadow-lg">
+              <img
+                src={item.image}
+                alt=""
+                className="w-full h-48 object-cover"
+              />
+              <div>{item.product_name}</div>
+              <div>{item.price}</div>
+              <button className="bg-blue-500 p-2">Add to Cart</button>
+            </div>
+          ))}
+        <div />
+      </div>
     </div>
   );
 }
---
+++ src/constant.js
@@ -0,0 +1,41 @@
+
+const product =[
+    {
+      "product_id": 1,
+      "product_name": "T-Shirt",
+      "image": "https://images.unsplash.com/photo-1523275335684-37898b6baf30?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZHVjdHxlbnwwfHwwfHx8MA%3D%3D",
+      "price": 19.99
+    },
+    {
+      "product_id": 2,
+      "product_name": "Coffee Mug",
+      "image": "https://images.unsplash.com/photo-1523275335684-37898b6baf30?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZHVjdHxlbnwwfHwwfHx8MA%3D%3D",
+      "price": 8.50
+    },
+    {
+      "product_id": 3,
+      "product_name": "Wireless Headphones",
+      "image": "https://images.unsplash.com/photo-1523275335684-37898b6baf30?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZHVjdHxlbnwwfHwwfHx8MA%3D%3D",
+      "price": 79.95
+    },
+    {
+      "product_id": 4,
+      "product_name": "Mouse ",
+      "image": "https://images.unsplash.com/photo-1523275335684-37898b6baf30?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZHVjdHxlbnwwfHwwfHx8MA%3D%3D",
+      "price": 7.95
+    },
+    {
+      "product_id": 5,
+      "product_name": "Key Board ",
+      "image": "https://images.unsplash.com/photo-1523275335684-37898b6baf30?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZHVjdHxlbnwwfHwwfHx8MA%3D%3D",
+      "price": 72.95
+    },
+    {
+      "product_id": 6,
+      "product_name": "Mobile Charger ",
+      "image": "https://images.unsplash.com/photo-1523275335684-37898b6baf30?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZHVjdHxlbnwwfHwwfHx8MA%3D%3D",
+      "price": 30.95
+    },
+  ]
+
+  export default product;
